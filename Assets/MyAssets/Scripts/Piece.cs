0c7b7c424746d9fb3b3f029e8255af605ddc18f2
TheFlamingoGames <theflamingogames@gmail.com>
2021-02-25 11:27:47 +0100

    Initial Commit
    New Project added
    
    Includes:
    -Unity URP project
    -Chess models
    -Wood Shaders

---
+++ Assets/MyAssets/Scripts/Piece.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    int[] pos = new int[2];
+
+    bool side;
+    //Dark or Light side
+    /* 0 == Light
+     * 1 == Dark
+     * */
+    public int[] GetPos()
+    {
+        return pos;
+    }
+
+    public void SetPosX(int x)
+    {
+        if (x >= 0 && x < 8)
+        {
+            pos[0] = x;
+        }
+    }
+
+    public void SetPosY(int y)
+    {
+        if (y >= 0 && y < 8)
+        {
+            pos[0] = y;
+        }
+    }
+
+    public void SetPos(int x, int y)
+    {
+        if (x < 0 || x > 7) return;
+        if (y < 0 || y > 7) return;
+
+        pos[0] = x;
+        pos[1] = y;
+    }
+
+    public bool GetSide() 
+    {
+        return side;
+    }
+
+    public void SetSide(bool newSide)
+    {
+        side = newSide;
+    }
+}
